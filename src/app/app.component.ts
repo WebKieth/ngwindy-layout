dd4f9111662bbb114aabb7b3542765b2e725afe7
nkhovdun <nikita.khovdun@novardis.com>
2019-08-07 19:02:59 +0300

    angular cli with postcss tailwind prebrewing configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import './app.component.scss';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'ng-windy';
